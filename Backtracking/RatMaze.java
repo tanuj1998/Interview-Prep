ce52f93bb27b8c77d03c70b80e95fe3d50be1fc6
tanuj1998 <tyadav@purdue.edu>
2019-06-26 17:50:28 -0700

    RatMaze description

--- Backtracking/RatMaze.java
+++ Backtracking/RatMaze.java
@@ -1,3 +1,9 @@
+/*
+A Maze is given as N*N binary matrix of blocks where source block is the upper left most block i.e., 
+maze[0][0] and destination block is lower rightmost block i.e., maze[N-1][N-1]. A rat starts from source 
+and has to reach the destination. The rat can move only in two directions: forward and down.
+Find the solution matrix
+*/
 public class RatMaze{
 	static int N;
 
