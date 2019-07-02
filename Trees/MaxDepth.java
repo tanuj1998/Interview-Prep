ae6ade2461aef21bf2b0fe3b28ebca4afe002a5b
tanuj1998 <tyadav@purdue.edu>
2019-07-01 18:29:19 -0700

    MaxDepth

---
+++ Trees/MaxDepth.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int maxDepth(TreeNode A) {
+        if(A == null){
+            return 0;
+        }
+        
+        int left = maxDepth(A.left);
+        int right = maxDepth(A.right);
+        
+        int ans = Math.max(left,right)+1;
+        return ans;
+    }
+}
