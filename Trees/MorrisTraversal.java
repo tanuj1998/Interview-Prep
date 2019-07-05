a4e2486105c12896d5469cd5958c1ddf99f547d6
tanuj1998 <tyadav@purdue.edu>
2019-07-04 21:16:01 -0700

    Morris

---
+++ Trees/MorrisTraversal.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+
+class MorrisTraversal{
+	public void morris(TreeNode root){
+		TreeNode curr;
+		TreeNode prev;
+		if(root == null){
+			return ;
+		}
+
+		curr = root;
+
+		while(curr != null){
+			if(curr.left == null){
+				System.out.print(curr.val + " ");
+				curr = curr.right;
+			}
+			else{
+				pre = curr.left;
+
+				while(pre.right != null && pre.right != curr){
+					pre = pre.right;
+
+					if (pre.right == null) { 
+                    pre.right = current; 
+                    current = current.left; 
+                } 
+                else { 
+                    pre.right = null; 
+                    System.out.print(curr.val + " "); 
+                    curr = curr.right; 
+                	}
+				}
+			}
+		}
+	}
+}
