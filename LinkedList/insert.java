0a4f478c9ce25419fd9220b4660df44e57105c1d
tanuj1998 <tyadav@purdue.edu>
2019-07-06 15:31:57 -0700

    insert at end

--- LinkedList/insert.java
+++ LinkedList/insert.java
@@ -16,3 +16,23 @@ public void insertAfter(Node prev_node, int new_data){
 	new_node.next = prev_node.next;
 	prev_node.next = new_node;
 }
+
+//Add at the end
+
+public void append(int new_data){
+	Node new_node = new Node(new_data);
+	
+	if(head == null){
+		head = new Node(new_data);
+		return;
+	}
+
+	new_node.next = null;
+
+	Node last = head;
+	while(last != null){
+		last = last.next;
+	}
+	last.next = new_node;
+	return;
+}
