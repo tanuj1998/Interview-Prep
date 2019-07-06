08acfe81c4c69650afde5c93e1b8c07951516d67
tanuj1998 <tyadav@purdue.edu>
2019-07-05 18:04:43 -0700

    SIP

---
+++ Binary Search/SortedInsertPosition.java
@@ -0,0 +1,35 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+*/
+public class SortedInsertPosition {
+    public int searchInsert(ArrayList<Integer> a, int b) {
+        int low = 0;
+        int high = a.size()-1;
+        int mid;
+        while(low <= high){
+            mid = (low+high)/2;
+            
+            if(a.get(mid) == b){
+                return mid;
+            }
+            else if(a.get(mid) < b){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return low;
+    }
+}
