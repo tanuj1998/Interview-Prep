95367dcbdd52daa199f5485dc068948d4ac36b75
tanuj1998 <tyadav@purdue.edu>
2019-07-10 17:04:03 -0700

    moveZeroes

---
+++ Arrays/moveZeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+    int check = 0;
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            nums[check] = nums[i];
+            check++;
+        }
+    }
+
+    for (int i = check; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+    }
+}
