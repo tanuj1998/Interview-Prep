aef512f80b07df38980b36393f912d92327f84c2
tanuj1998 <tyadav@purdue.edu>
2019-07-20 12:51:41 -0700

    wave2

--- Arrays/WaveArray.java
+++ Arrays/WaveArray.java
@@ -1,3 +1,9 @@
+/*
+Given an array of integers, sort the array into a wave like array and return it, 
+In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+
+
+*/
 public class Solution {
     
     public ArrayList<Integer> wave(ArrayList<Integer> A) {
