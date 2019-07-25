d47fc927d10ef77ae82c35e30b8f8fe591557943
tanuj1998 <tyadav@purdue.edu>
2019-07-25 13:42:01 -0700

    palindrome3

--- LinkedList/Palindrome.java
+++ LinkedList/Palindrome.java
@@ -1,6 +1,13 @@
 /*
 Given a singly linked list, determine if it is a palindrome.
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
 /*
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
