d17122577ccf61ec51c71025119213523895627e
tanuj1998 <tyadav@purdue.edu>
2019-08-06 10:55:32 -0700

    LRU Cache 1

---
+++ Heaps&Maps/LRUCache.java
@@ -0,0 +1,10 @@
+/*
+Design and implement a data structure for LRU (Least Recently Used) cache. It should support the following operations: get and set.
+
+get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+set(key, value) - Set or insert the value if the key is not already present. When the cache reaches its capacity, it 
+should invalidate the least recently used item before inserting the new item.
+The LRU Cache will be initialized with an integer corresponding to its capacity. Capacity indicates the maximum 
+number of unique keys it can hold at a time.
+
+*/
