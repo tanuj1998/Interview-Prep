295de9db4b2bf8384c1b9b1b12359552fea26e36
tanuj1998 <tyadav@purdue.edu>
2019-08-10 15:52:38 -0400

    tilt 2

--- Trees/Tilt.java
+++ Trees/Tilt.java
@@ -7,3 +7,20 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Solution {
+    int ans = 0;
+    public int findTilt(TreeNode root) {
+      helper(root);
+      return ans;
+    }
+    public int helper(TreeNode temp){
+        if(temp == null){
+            return 0;
+        }
+        int l = helper(temp.left);
+        int r = helper(temp.right);
+        ans += Math.abs(l-r);
+        
+        return l+r+temp.val;
+    }
+}
