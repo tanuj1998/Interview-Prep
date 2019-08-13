fdea13b8767ee3cca1971ac7db0137e94d7f5357
tanuj1998 <tyadav@purdue.edu>
2019-08-13 13:52:01 -0400

    product except itself 2

--- Arrays/ProductExceptItself.java
+++ Arrays/ProductExceptItself.java
@@ -2,3 +2,29 @@
 Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to 
 the product of all the elements of nums except nums[i].
 */
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int len = nums.length;
+        
+        int[] left = new int[len];
+        int[] right = new int[len];
+        int ans[] = new int[len];
+        
+        left[0] = 1;
+        for(int i = 1; i < len; i++){
+            left[i] = nums[i-1]*left[i-1];
+        }
+        
+        right[len-1] = 1;
+        for(int i = len-2; i >= 0; i--){
+            right[i] = nums[i+1]*right[i+1];
+        }
+
+        for(int i = 0; i < len; i++){
+        ans[i] = left[i]*right[i];
+        }
+        return ans;
+    }
+}
