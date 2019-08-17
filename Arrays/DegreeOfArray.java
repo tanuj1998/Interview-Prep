6c04177fa221b80ca94fe248b46a264adfb744df
tanuj1998 <tyadav@purdue.edu>
2019-08-17 18:12:02 -0400

    degree of array 2

--- Arrays/DegreeOfArray.java
+++ Arrays/DegreeOfArray.java
@@ -1,3 +1,10 @@
+/*
+Given a non-empty array of non-negative integers nums, the degree of this array is defined 
+as the maximum frequency of any one of its elements.
+
+Your task is to find the smallest possible length of a (contiguous) 
+subarray of nums, that has the same degree as nums.
+*/
 class Solution {
     public int findShortestSubArray(int[] nums) {
      Map<Integer, Integer> left = new HashMap();
