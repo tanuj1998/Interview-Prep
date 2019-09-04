d03a852d8502596e65a1de34dd4df7ba8d8494de
tanuj1998 <tyadav@purdue.edu>
2019-09-04 15:36:46 -0400

    Verifying Alien Dictionary2

--- String/VerifyingAlienDictionary.java
+++ String/VerifyingAlienDictionary.java
@@ -1,3 +1,10 @@
+/*
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. 
+The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only
+ if the given words are sorted lexicographicaly in this alien language.
+*/
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
         int[] index = new int[26];
