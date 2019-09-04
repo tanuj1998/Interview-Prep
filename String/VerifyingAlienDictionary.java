3b9a4cb2dfe6e81b975722ceabf2ea644e64f66c
tanuj1998 <tyadav@purdue.edu>
2019-09-04 15:37:33 -0400

    Verifying Alien Dictionary4

--- String/VerifyingAlienDictionary.java
+++ String/VerifyingAlienDictionary.java
@@ -16,6 +16,13 @@ Example 2:
 Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
 Output: false
 Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical 
+rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
 */
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
