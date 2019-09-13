9c12408e7bee3098d62d2d3b99f2807942d6eb84
tanuj1998 <tyadav@purdue.edu>
2019-09-12 20:32:59 -0400

    Search Rotated Array3

--- Binary Search/SearchInSortedRotatedArray.java
+++ Binary Search/SearchInSortedRotatedArray.java
@@ -8,6 +8,15 @@ You are given a target value to search. If found in the array return its index, 
 You may assume no duplicate exists in the array.
 
 Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
 */
 class Solution {
     public int search(int[] A, int target) {
