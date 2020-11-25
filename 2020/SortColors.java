bd4495d65e12a294385cf521256e990f9ff8791e
tanuj1998 <tyadav@purdue.edu>
2020-11-24 22:24:28 -0500

    added

---
+++ 2020/SortColors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int i = 0;
+        
+        while(i <= right && left < right){
+            if(nums[i] == 0){
+                nums[i] = nums[left];
+                nums[left] = 0;
+                left++;
+                i++;
+            }
+            
+            else if(nums[i] == 2){
+                nums[i] = nums[right];
+                nums[right] = 2;
+                right--;
+            }
+            
+            else{
+                i++;
+            }
+        }
+    }
+}
