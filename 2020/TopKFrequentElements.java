05b00b57bb1d74ae004e5e299784360ca94bff1b
tanuj1998 <tyadav@purdue.edu>
2020-11-27 12:39:50 -0500

    add

---
+++ 2020/TopKFrequentElements.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length) {
+            return nums;
+        }
+        
+        Map<Integer, Integer> count = new HashMap();
+        for (int n: nums) {
+          count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
+
+        for (int n: count.keySet()) {
+          heap.add(n);
+          if (heap.size() > k){
+              heap.poll();  
+          }
+        }
+
+        int[] ans = new int[k];
+        for(int i = k - 1; i >= 0; i--){
+            ans[i] = heap.poll();
+        }
+        return ans;
+    }
+}
